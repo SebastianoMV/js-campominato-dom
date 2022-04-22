01c6fc0a7dcf2ff3b5f06543434d96fea6563f76
SebastianoMV <sebastianovisco@gmail.com>
2022-04-22 16:00:54 +0200

    Prima versione senza bonus

--- script.js
+++ script.js
@@ -1,24 +1,31 @@
 document.querySelector('#start').addEventListener('click',init);
 const NumeroBombe = 16;
+const container = document.querySelector('.smv-container');
+const main = document.querySelector('main');
+let Bombe =[];
+let c = 0;
 
 function init(){
-  
-  const container = document.querySelector('.smv-container');
-  const difficolta = document.getElementById('difficolta').value;
   container.innerHTML = '';
+  const difficolta = document.getElementById('difficolta').value;
+  createGame(difficolta);
+  
+}
 
-  const Bombe = generateBombe(difficolta);
-  let c = 0;
-  for(let i = 0; i < difficolta; i++){
+function createGame(difficolta){
+   Bombe = generateBombe(difficolta);
+   console.log(Bombe);
+   for(let i = 0; i < difficolta; i++){
      const sq = createSquare(container,difficolta)
      sq.innerHTML = `<span>${i}</span>`;
-     sq.addEventListener('click', ClickedFun)
+     sq.numero = i;
+     sq.addEventListener('click', ClickedFun);
    }
 }
 
-function createSquare( target , value ){
-    const sq = document.createElement( 'div' );
-    sq.className = 'square square'+ value;
+function createSquare( target , difficolta ){
+    const sq = document.createElement('div');
+    sq.className = 'square square'+ difficolta;
     target.append(sq);
     return sq;
 }
@@ -27,7 +34,7 @@ function generateBombe(difficolta){
    const GeneraBombe=[];
    while (GeneraBombe.length < NumeroBombe){
      const bombe = random(1,difficolta)
-     console.log(bombe);
+     
      if(!GeneraBombe.includes(bombe)){
         GeneraBombe.push(bombe)
      }
@@ -40,9 +47,20 @@ function random(min,max){
 }
 
 function ClickedFun(){
-   this.classList.add('clicked');
+   
+   console.log('numero' + this.numero);
+   
+   if (Bombe.includes(this.numero)){
+      this.classList.add('bomba');
+      document.querySelector('h2').innerHTML = 'Hai perso!'
+   }else{
+       this.classList.add('clicked');
+       c++;
+       document.querySelector('h3').innerHTML = `Punteggio:${c} `
+   }
+
 }
 
-function endGame(){
+function endGame(difficolta){
    
 }
